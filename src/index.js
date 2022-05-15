706ef9f5b0e975ada1e5ef70297f2646d2cb56ae
ntsubasao <71168443+ntsubasao@users.noreply.github.com>
2022-05-15 22:25:17 +0900

    javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value; //inputに入力された値だから、valueを使
+  document.getElementById("add-text").value = ""; //inputTextの内容を空にする
+
+  //div生成　DOM
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  console.log(div);
+
+  //未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button") //idにマッチするDocument要素を取得する
+  .addEventListener("click", () => onClickAdd()); //イベント(ボタンを押したなど)を検知したら、onClickを返す
