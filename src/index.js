b559f2933b0d2dc89cf7899336d0564356907774
ntsubasao <71168443+ntsubasao@users.noreply.github.com>
2022-05-22 11:11:41 +0900

    totyuu

--- src/index.js
+++ src/index.js
@@ -13,8 +13,24 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText; //中身をliタグの中に値を入れられる
 
-  //divタグの子要素に各要素を設定
+  //button（完了）作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button（削除）作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  //divタグの子要素に各要素を設定(appendchildはどんどん下に追加される)
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了リストに追加 （incomplete-list の子要素に）
   document.getElementById("incomplete-list").appendChild(div);
